5b773d5c39b471523cc609c758bf4c02fdf7ed8f
Dan Dudley <born2snipe@gmail.com>
2012-11-26 21:34:53 -0600

    made plugin

---
+++ README.md
@@ -0,0 +1,37 @@
+# Maven Build Time Plugin
+
+This maven plugin provides goals to start/stop a timer, when the timer is stopped it notifies listeners of the elapsed time and the project that was built.
+
+## How do I implement a listener?
+
+   - Create a new maven module and add the plugin's jar as a dependency
+
+    <depenendcy>
+        <groupId>b2s</groupId>
+        <artifactId>maven-plugin-build-time</artifactId>
+        <version>X.X.X</version>
+        <type>jar</type>
+        <scope>provided</scope>
+    </depenendcy>
+
+  - Now implement the TimerListener interface
+
+  - For the plugin to discover your listener you need to use the [@ServiceProvider](http://bits.netbeans.org/dev/javadoc/org-openide-util-lookup/org/openide/util/lookup/ServiceProvider.html) annotation
+
+## How do I add a listener to the plugin?
+
+   All you need to do is add the module that contains the listener as a dependency to the plugin
+
+   <plugin>
+        <groupId>b2s</groupId>
+        <artifactId>maven-plugin-build-time</artifactId>
+        <version>X.X.X</version>
+        <dependencies>
+          <dependency>
+            <groupId>custom-listener</groupId>
+            <artifactId>custom-listener</artifactId>
+            <version>Y.Y.Y</version>
+          </dependency>
+        </dependencies>
+    </plugin>
+
---
+++ src/main/java/org/apache/maven/plugin/StopTimerMojo.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * Copyright to the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package org.apache.maven.plugin;
+
+import org.apache.maven.project.MavenProject;
+import org.openide.util.Lookup;
+
+import java.util.Collection;
+
+/**
+ * Stop the stop watch for the project being built and notify the notifiers
+ *
+ * @goal stop
+ */
+public class StopTimerMojo extends AbstractMojo {
+    private StopWatchProvider stopWatchProvider = new StopWatchProvider();
+    private Lookup lookup = Lookup.getDefault();
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        StopWatch stopWatch = stopWatchProvider.get();
+        stopWatch.stop();
+
+        Collection<? extends TimerListener> buildTimeListeners = lookup.lookupAll(TimerListener.class);
+        if (buildTimeListeners.isEmpty()) {
+            getLog().warn("No instance(s) of " + TimerListener.class.getName() + " were provided");
+        }
+
+        MavenProject project = (MavenProject) getPluginContext().get("project");
+        for (TimerListener buildTimeListener : buildTimeListeners) {
+            buildTimeListener.onStop(stopWatch.getElapsedTime(), project, getLog());
+        }
+    }
+}
